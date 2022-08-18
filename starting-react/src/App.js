a824f84a88a614191ca37578b19f59ae7d687f15
unosinim <unosinim@gmail.com>
2022-08-18 00:34:09 -0500

    Changes to pokemon search

--- starting-react/src/App.js
+++ starting-react/src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+
 import './App.css';
+import pokemon from './pokemon.json';
+
+const PokemonRow = ({pokemon}) => (
+    <tr>
+      <td>{pokemon.name.english}</td>
+      <td>{pokemon.type.join(', ')}</td>
+    </tr>
+);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React Loaded
-        </a>
-      </header>
+    <div style={{
+      margin: "auto",
+      width: 800,
+      paddingTop: "1rem"
+
+    }
+
+    }>
+      <h1 className="title">Pokemon Search</h1>
+
+      <table width="100%">
+
+        <thead>
+
+          <tr>
+            <th>Name</th>
+            <th>Type</th>
+          </tr>
+        </thead>
+        <tbody>
+          {pokemon.map((pokemon) => (
+            <PokemonRow pokemon={pokemon} key ={pokemon.id}/>
+          ))}
+        </tbody>
+      </table>
+
+
     </div>
   );
 }
